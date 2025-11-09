4e522961193dc07f9711834727fa9ef5bb03f446
shaobai-li <shaobai.ai@gmail.com>
2025-11-09 23:54:45 +0800

    refactor(backend): restructure project into modular Tauri Rust backend (main.rs + commands.rs)

--- src-tauri/src/command.rs
+++ src-tauri/src/command.rs
@@ -1,16 +1,5 @@
-// src-tauri/src/lib.rs
-
-use log::LevelFilter;
-
-// 定义一个可以被前端调用的命令
 #[tauri::command]
-fn fixed_chat_reply(content: String) -> String {
-    println!("收到前端消息: {}", content);
-    format!("这是来自 Rust 的回复：{}", content)
-}
-
-#[tauri::command]
-async fn chat_reply(content: String) -> Result<String, String> {
+pub async fn chat_reply(content: String) -> Result<String, String> {
     println!("收到前端消息: {}", content);
 
     use dotenvy::dotenv;
@@ -39,15 +28,4 @@ async fn chat_reply(content: String) -> Result<String, String> {
 
     let text = resp.text().await.map_err(|e| e.to_string())?;
     Ok(text)
-}
-
-
-
-#[cfg_attr(mobile, tauri::mobile_entry_point)]
-pub fn run() {
-    tauri::Builder::default()
-        // 注册前端可以调用的命令
-        .invoke_handler(tauri::generate_handler![chat_reply])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,41 +1,11 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// use dotenvy::dotenv;
-// use std::env;
-// use reqwest::Client;
-// use serde_json::json;
+mod command;
 
-// #[tokio::main]
-// async fn main() -> Result<(), Box<dyn std::error::Error>> {
-//     dotenv().ok();
-
-//     let api_key = env::var("DEEPSEEK_API_KEY").expect("DEEPSEEK_API_KEY not found");
-//     let client = Client::new();
-//     let url = "https://api.deepseek.com/v1/chat/completions";
-
-//     // 你的输入
-//     let prompt = "请用一句话解释量子纠缠";
-
-//     let body = json!({
-//         "model": "deepseek-chat",
-//         "messages": [
-//             {"role": "user", "content": prompt}
-//         ]
-//     });
-
-//     let resp = client
-//         .post(url)
-//         .bearer_auth(&api_key)
-//         .json(&body)
-//         .send()
-//         .await?;
-
-//     let text = resp.text().await?;
-//     println!("DeepSeek Response:\n{}", text);
-
-//     Ok(())
-// }
-
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
 fn main() {
-    app_lib::run();
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![command::chat_reply])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
